90313f6f7a23967898b8109ac6c5e6fb0016adf3
Ricky <rickykusriana@gmail.com>
2020-10-19 15:10:57 +0700

    Update

---
+++ precache-manifest.4eed1adceb6ebf5db4701dc8b01c2db5.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e11b376c7229d396613453657a23571",
+    "url": "/assets/demo/css/site.css"
+  },
+  {
+    "revision": "a09e13ee94d51c524b7e2a728c7d4039",
+    "url": "/assets/demo/js/jquery-3.3.1.min.js"
+  },
+  {
+    "revision": "3ca827587c6488c673ab5374c1c96c5a",
+    "url": "/assets/metro-4.2.19/css/metro-all.min.css"
+  },
+  {
+    "revision": "aa7a32cfd99a4ca32f483f8546bb9eda",
+    "url": "/assets/metro-4.2.19/mif/metro.json"
+  },
+  {
+    "revision": "f2575503239d0a836b0f137b47a25621",
+    "url": "/assets/metro-4.2.19/mif/metro.svg"
+  },
+  {
+    "revision": "c95161918e2aea18da5e9c11dd42fd7a",
+    "url": "/assets/metro-4.2.19/mif/metro.ttf"
+  },
+  {
+    "revision": "14b29bcc626b2b566c2a7a9f94e04718",
+    "url": "/assets/metro-4.2.19/mif/metro.woff"
+  },
+  {
+    "revision": "d593a9f72d37a6975b8588945fe46698",
+    "url": "/icon.png"
+  },
+  {
+    "revision": "63c292322fc261f42f327029ab1be624",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6df375f4da41f5fa1b78",
+    "url": "/js/app.69b9b9fc.js"
+  },
+  {
+    "revision": "38f70a9e2d6f979b5965",
+    "url": "/js/chunk-vendors.a7b2cc36.js"
+  },
+  {
+    "revision": "42508eb0800a369abd436dd4dfa6c369",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "d41d8cd98f00b204e9800998ecf8427e",
+    "url": "/sw.js"
+  }
+]);
---
+++ sw.js
@@ -0,0 +1,3 @@
+importScripts("/precache-manifest.4eed1adceb6ebf5db4701dc8b01c2db5.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+
